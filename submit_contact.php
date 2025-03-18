6faf7ad16de62c14a505fd95c8a65893b642f3ab
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2025-03-18 15:12:28 +0000

    Recuperation des donner avec $_GET[' ' ]

---
+++ submit_contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<h1>Message bien reçu !</h1>
+        
+        <div class="card">
+            
+            <div class="card-body">
+                <h5 class="card-title">Rappel de vos informations</h5>
+                <p class="card-text"><b>Email</b> : <?php echo $_GET['email']; ?></p>
+                <p class="card-text"><b>Message</b> : <?php echo $_GET['message']; ?></p>
+            </div>
+        </div>
+</body>
+</html>
