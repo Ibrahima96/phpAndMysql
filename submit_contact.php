f6d2b1560abcdaac676131423b9ea7826d53fc94
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2025-03-20 00:12:09 +0000

    L'envoie des donnee ou de fichier

--- submit_contact.php
+++ submit_contact.php
@@ -21,6 +21,7 @@ if (
 }
 ;?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -47,3 +48,34 @@ if (
 
 </body>
 </html>
+<?php
+$isFileLoaded = false;
+// Testons si le fichier a bien été envoyé et s'il n'y a pas des erreurs
+if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] === 0) {
+    // Testons, si le fichier est trop volumineux
+    if ($_FILES['screenshot']['size'] > 1000000) {
+        echo "L'envoi n'a pas pu être effectué, erreur ou image trop volumineuse";
+        return;
+    }
+
+    // Testons, si l'extension n'est pas autorisée
+    $fileInfo = pathinfo($_FILES['screenshot']['name']);
+    $extension = $fileInfo['extension'];
+    $allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
+    if (!in_array($extension, $allowedExtensions)) {
+        echo "L'envoi n'a pas pu être effectué, l'extension {$extension} n'est pas autorisée";
+        return;
+    }
+
+    // Testons, si le dossier uploads est manquant
+    $path = __DIR__ . '/uploads/';
+    if (!is_dir($path)) {
+        echo "L'envoi n'a pas pu être effectué, le dossier uploads est manquant";
+        return;
+    }
+
+    // On peut valider le fichier et le stocker définitivement
+    move_uploaded_file($_FILES['screenshot']['tmp_name'], $path . basename($_FILES['screenshot']['name']));
+    $isFileLoaded = true;
+}
+?>
